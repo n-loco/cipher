cc6f649540643e8e365e880dcfea725eb267af21
N_Loco <nlocogm@gmail.com>
2022-04-27 22:55:01 -0300

    Service Worker

---
+++ sw.js
@@ -0,0 +1,47 @@
+const stccacheName = "cipherCache_04_27_2022"
+
+this.addEventListener('install', (event) => {
+
+    this.skipWaiting()
+
+    event.waitUntil(
+
+        caches.open(stccacheName).then((cache) => {
+
+            return cache.addAll([
+                './index.html',
+                './ui/window.css'
+            ])
+
+        })
+
+    )
+
+})
+
+this.addEventListener('activate', (event) => {
+
+    event.waitUntil(
+
+        caches.keys().then( (cacheNames) => {
+
+            return cacheNames
+                        .filter((cache) => (cache.startsWith("cipherCache_")))
+                        .filter((cache) => (cache !== stccacheName))
+                        .map((cache) => (caches.delete(cache)))
+
+        })
+
+    )
+
+})
+
+this.addEventListener('fetch', (event) => {
+
+    return fetch(event.request)
+
+})
+
+/*
+If this code is weird, it's because I don't really know how to make a service worker
+*/
