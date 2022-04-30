56c8042bce34af6341d89f884f769570836d1bc0
N_Loco <nlocogm@gmail.com>
2022-04-30 17:37:13 -0300

    Write text

--- js/shrd.js
+++ js/shrd.js
@@ -9,18 +9,33 @@ const CCP = document.querySelector('#ccp')
 const CCLR = document.querySelector('#cclr')
 
 
-CRIP.onclick = () => encrypt(TXTF, TXTFC)
+CRIP.onclick = () => encrypt(TXTF, TXTFC, -1)
 DCCP.onclick = () => copy(TXTF)
 DCCLR.onclick = () => cleararea(TXTF)
 
-DCRIP.onclick = () => encrypt(TXTFC, TXTF)
+DCRIP.onclick = () => encrypt(TXTFC, TXTF, 1)
 CCP.onclick = () => copy(TXTFC)
 CCLR.onclick = () => cleararea(TXTFC)
 
 
-function encrypt(input, output) {
+function encrypt(input, output, op = -1) {
 
-    output.value = `TEST( ${input.value} )`
+    output.value = ''
+
+    const CHARSET = [
+
+        '1234567890',
+        'qwertyuiop',
+        'asdfghjkl',
+        'zxcvbnm'
+
+    ]
+
+    for ( char in input.value ) {
+
+        output.value += input.value[char]
+
+    }
 
 }
 
